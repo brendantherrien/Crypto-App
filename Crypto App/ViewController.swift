cb16c650a15e891fe1ddab7bb03e38469b06d9b6
Brendan Therrien <brendant98@icloud.com>
2020-08-27 10:06:26 -0400

    initial commit

---
+++ Crypto App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crypto App
+//
+//  Created by Brendan Therrien on 8/27/20.
+//  Copyright © 2020 Brendan Therrien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
